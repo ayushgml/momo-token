ba7cf7b6db4df42756ca7460792e3255056d2143
ayushgml <ayushgml@gmail.com>
2022-06-03 23:07:03 +0530

    updated contract and js code for MomoToken

--- contracts/MomoToken.sol
+++ contracts/MomoToken.sol
@@ -8,11 +8,23 @@ contract MomoToken {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 value
+    );
+
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
     
-    
-    
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+    }
 }
