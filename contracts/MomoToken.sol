bac959ba418938adcdfe278e72e177e9e8840d53
ayushgml <ayushgml@gmail.com>
2022-06-04 22:42:58 +0530

    still working on the error

--- contracts/MomoToken.sol
+++ contracts/MomoToken.sol
@@ -20,11 +20,13 @@ contract MomoToken {
     }
     
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        
         require(balanceOf[msg.sender] >= _value);
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
